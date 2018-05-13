0f20c97fabc3aca9ea0b0e290da3e1a32df92985
LEGOLLAS <gurwl1885@gmail.com>
2018-05-14 02:00:58 +0900

    login

---
+++ js/server.js
@@ -0,0 +1,10 @@
+const express = require('http');
+const port = process.env.PORT || 3000;
+const router = require('./route/Router');
+
+app.use(router)
+
+app.listen(port,err =>{
+    if(err) console.log(err)
+    else console.log("서버가 가동되었습니다.")
+});
